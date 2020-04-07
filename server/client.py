2ec296a31dafd12526994f4272afe87f5eae1963
Jonas Vander Vennet <jonas.vdv@hotmail.com>
2020-04-07 19:48:57 +0200

    Testing client to go along with basic server

---
+++ server/client.py
@@ -0,0 +1,22 @@
+import logging
+
+import grpc
+
+import expenses_pb2
+import expenses_pb2_grpc
+from datetime import datetime
+
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = expenses_pb2_grpc.ExpensesStub(channel)
+        # response = stub.CreateOneExpense(expenses_pb2.Expense(winkelID="fnac gent", price=9.5, timestamp=str(datetime.now()), summary="yolotanker"))
+        # response = stub.GetMultiExpenses(expenses_pb2.MultiExpensesRequest())
+        response = stub.GetOneExpense(expenses_pb2.ExpenseRequest())
+    print(response)
+    # print(len(response.expenses))
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
