15644b268a647a2feca3781d8a4cf552f6692cd2
Jonas Vander Vennet <jonas.vdv@hotmail.com>
2020-04-15 11:19:37 +0200

    Rename App

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -5,37 +5,24 @@ import 'package:grpc/grpc.dart';
 import 'proto-compiled/expenses.pb.dart';
 import 'proto-compiled/expenses.pbgrpc.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ExpenseTracker());
 
-class MyApp extends StatelessWidget {
+class ExpenseTracker extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Expense tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Expense tracker'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
@@ -49,7 +36,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _yolotanker = "yolo";
   List<Card> _expenses = List<Card>();
 
   Card _convertToCard(Expense expense) {
--- ui/test/widget_test.dart
+++ ui/test/widget_test.dart
@@ -10,10 +10,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ui/main.dart';
 
+// TODO: test
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ExpenseTracker());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
